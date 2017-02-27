b8b4ca168efc97813bafa5f4c77f3a35dcbebeda
Mario Döring <deluna.yy@gmail.com>
2017-02-27 20:56:24 +0100

    Added doublicated line break test

--- tests/ContainerParser/ContainerLexerTest.php
+++ tests/ContainerParser/ContainerLexerTest.php
@@ -2,11 +2,29 @@
 namespace ClanCats\Container\Tests\ContainerParser;
 
 use ClanCats\Container\ContainerParser\{
-    ContainerLexer
+    ContainerLexer,
+    Token as T
 };
 
 class ContainerLexerTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * Assert the token types
+     */
+    protected function assertTokenTypes(string $code, array $expected)
+    {
+        $actualTokens = [];
+
+        $lexer = new ContainerLexer($code);
+
+        foreach($lexer->tokens() as $token)
+        {
+            $actualTokens[] = $token->getType();
+        }
+
+        $this->assertEquals($expected, $actualTokens);
+    }
+
     public function testConstruct()
     {
         $lexer = new ContainerLexer('test');
@@ -19,5 +37,15 @@ class ContainerLexerTest extends \PHPUnit_Framework_TestCase
         // trim
         $lexer = new ContainerLexer(' test ');
         $this->assertEquals('test', $lexer->code());
+    }
+
+    public function testDoublicatedLinebreaks()
+    {
+        $this->assertTokenTypes("yes\n\n\nno", 
+        [
+            T::TOKEN_BOOL_TRUE,
+            T::TOKEN_LINE,
+            T::TOKEN_BOOL_FALSE,
+        ]);
     }
 }
