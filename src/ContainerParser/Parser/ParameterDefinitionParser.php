6709f424f81c82c3b519ef81bd3b2f58ae8bb7cf
Mario Döring <deluna.yy@gmail.com>
2017-03-29 22:41:27 +0200

    Parameter definition parser dummy

--- src/ContainerParser/Parser/ParameterDefinitionParser.php
+++ src/ContainerParser/Parser/ParameterDefinitionParser.php
@@ -9,6 +9,43 @@ use ClanCats\Container\ContainerParser\{
     
 };
 
-class ParameterDefinitionNode extends ContainerParser
+class ParameterDefinitionParser extends ContainerParser
 {
+	/**
+     * The current scope node
+     * 
+     * @param ScopeNode
+     */
+    protected $param;
+
+    /**
+     * Prepare the current parser 
+     * 
+     * @return void
+     */
+    protected function prepare() 
+    {
+       
+    }
+
+    /**
+     * Return the current result
+     * 
+     * @return null|Node
+     */
+    protected function node() : Node
+    {
+        return new Node();
+    }
+
+    /**
+     * Parse the next token
+     *
+     * @return null|Node
+     */
+    protected function next()
+    {
+        $this->skipToken();
+        //var_dump($this->currentToken()); die;
+    }
 }
