7eccb95b707b50acebaf73c570e8c86132546e24
Mario Döring <deluna.yy@gmail.com>
2017-04-16 13:46:09 +0200

    Added container interpreter basics

--- src/ContainerParser/ContainerInterpreter.php
+++ src/ContainerParser/ContainerInterpreter.php
@@ -8,8 +8,68 @@
  */
 namespace ClanCats\Container\ContainerParser;
 
+use ClanCats\Container\Exceptions\ContainerInterpreterException;
+
+use ClanCats\Container\ContainerNamespace;
+
+use ClanCats\Container\ContainerParser\Nodes\{
+    BaseNode as Node,
+
+    // the nodes
+    ScopeNode,
+    ParameterDefinitionNode
+};
+
 class ContainerInterpreter
 {
-    
+    /**
+     * The container definitions namesapce 
+     * 
+     * @var ContainerNamespace
+     */
+    protected $namespace;
+
+    /**
+     * Construct a new container file interpreter
+     * 
+     * @param ContainerNamespace                $namespace
+     * @return void
+     */
+    public function __construct(ContainerNamespace $namespace)
+    {
+        $this->namespace = $namespace;
+    }
+
+	/**
+	 * Handle a container file scope
+	 * 
+	 * @param ScopeNode 			$scope
+	 * @return void
+	 */
+    public function handleScope(ScopeNode $scope) 
+    {
+    	foreach($scope->getNodes() as $node)
+    	{
+    		if ($node instanceof ParameterDefinitionNode)
+    		{
+    			$this->handleParameterDefinition($node);
+    		}
+    		else 
+    		{
+    			throw new ContainerInterpreterException("Unexpected node in scope found.");
+    		}
+    	}
+    }
+
+    /**
+     * Handle a parameter definition
+     * 
+     * @param ParameterDefinitionNode 			$definition
+     * @return void
+     */
+    public function handleParameterDefinition(ParameterDefinitionNode $definition) 
+    {
+        $this->namespace->setParameter($definition->getname(), $definition->getValue()->getRawValue());
+    }
 }
 
